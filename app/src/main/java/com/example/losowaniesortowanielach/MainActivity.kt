cb8cff4822a648b530c37479afc6edde8571e83d
kamillachdev <kamillachdev@gmail.com>
2022-11-28 15:01:14 +0100

    Random button action

--- app/src/main/java/com/example/losowaniesortowanielach/MainActivity.kt
+++ app/src/main/java/com/example/losowaniesortowanielach/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.losowaniesortowanielach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +22,24 @@ class MainActivity : AppCompatActivity() {
         val s7 = findViewById<Switch>(R.id.switch7)
         val s8 = findViewById<Switch>(R.id.switch8)
         val s9 = findViewById<Switch>(R.id.switch9)
+
+        //Buttons
+        val sortButton = findViewById<Button>(R.id.sortButton)
+        val randomButton = findViewById<Button>(R.id.randomButton)
+
+        //Text view
+        val sortedNumbersOutput = findViewById<TextView>(R.id.sortedNumbersText)
+
+        randomButton.setOnClickListener {
+            s1.text = Random.nextInt(0, 99).toString()
+            s2.text = Random.nextInt(0, 99).toString()
+            s3.text = Random.nextInt(0, 99).toString()
+            s4.text = Random.nextInt(0, 99).toString()
+            s5.text = Random.nextInt(0, 99).toString()
+            s6.text = Random.nextInt(0, 99).toString()
+            s7.text = Random.nextInt(0, 99).toString()
+            s8.text = Random.nextInt(0, 99).toString()
+            s9.text = Random.nextInt(0, 99).toString()
+        }
     }
 }
