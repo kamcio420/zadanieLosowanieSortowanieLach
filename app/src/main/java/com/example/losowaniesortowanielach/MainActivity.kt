ef81ca081286d0414ccaf56256ba25c6494243e9
kamillachdev <kamillachdev@gmail.com>
2022-11-28 15:24:33 +0100

    Sort button action, sorting function

--- app/src/main/java/com/example/losowaniesortowanielach/MainActivity.kt
+++ app/src/main/java/com/example/losowaniesortowanielach/MainActivity.kt
@@ -30,6 +30,27 @@ class MainActivity : AppCompatActivity() {
         //Text view
         val sortedNumbersOutput = findViewById<TextView>(R.id.sortedNumbersText)
 
+        //Sorting function
+        fun sorting(arr:ArrayList<Int>)
+        {
+            var swap = true
+            while(swap)
+            {
+                swap = false
+                for(i in 0 until arr.size-1)
+                {
+                    if(arr[i] > arr[i+1])
+                    {
+                        val temp = arr[i]
+                        arr[i] = arr[i+1]
+                        arr[i + 1] = temp
+                        swap = true
+                    }
+                }
+            }
+        }
+
+        //Random button action
         randomButton.setOnClickListener {
             s1.text = Random.nextInt(0, 99).toString()
             s2.text = Random.nextInt(0, 99).toString()
@@ -40,6 +61,48 @@ class MainActivity : AppCompatActivity() {
             s7.text = Random.nextInt(0, 99).toString()
             s8.text = Random.nextInt(0, 99).toString()
             s9.text = Random.nextInt(0, 99).toString()
+        }
+
+        //Sort button action
+        sortButton.setOnClickListener {
+            val numbers = ArrayList<Int>()
+
+            sortedNumbersOutput.text = ""
+
+            if(s1.isChecked){
+                numbers.add(s1.text.toString().toInt())
+            }
+            if(s2.isChecked){
+                numbers.add(s2.text.toString().toInt())
+            }
+            if(s3.isChecked){
+                numbers.add(s3.text.toString().toInt())
+            }
+            if(s4.isChecked){
+                numbers.add(s4.text.toString().toInt())
+            }
+            if(s5.isChecked){
+                numbers.add(s5.text.toString().toInt())
+            }
+            if(s6.isChecked){
+                numbers.add(s6.text.toString().toInt())
+            }
+            if(s7.isChecked){
+                numbers.add(s7.text.toString().toInt())
+            }
+            if(s8.isChecked){
+                numbers.add(s8.text.toString().toInt())
+            }
+            if(s9.isChecked){
+                numbers.add(s9.text.toString().toInt())
+            }
+
+            sorting(numbers)
+
+            for(elem in numbers)
+            {
+                sortedNumbersOutput.text = sortedNumbersOutput.text.toString() + elem.toString() + ", "
+            }
         }
     }
 }
